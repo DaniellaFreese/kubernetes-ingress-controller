5647263abcb5564f4ddb62a598b0c7349eae8536
Travis Raines <571832+rainest@users.noreply.github.com>
2022-06-30 17:01:39 -0700

    test(e2e) add a DB-less Kuma test

--- test/e2e/all_in_one_test.go
+++ test/e2e/all_in_one_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/blang/semver/v4"
 	"github.com/kong/kubernetes-testing-framework/pkg/clusters"
 	"github.com/kong/kubernetes-testing-framework/pkg/clusters/addons/kong"
+	"github.com/kong/kubernetes-testing-framework/pkg/clusters/addons/kuma"
 	"github.com/kong/kubernetes-testing-framework/pkg/clusters/addons/loadimage"
 	"github.com/kong/kubernetes-testing-framework/pkg/clusters/addons/metallb"
 	"github.com/kong/kubernetes-testing-framework/pkg/environments"
@@ -117,6 +118,64 @@ func TestDeployAllInOneDBLESS(t *testing.T) {
 	killKong(ctx, t, env, &pod)
 
 	t.Log("confirming that routes are restored after crash")
+	verifyIngress(ctx, t, env)
+}
+
+func TestDeployAllInOneDBLESSKuma(t *testing.T) {
+	t.Log("configuring all-in-one-dbless.yaml manifest test")
+	t.Parallel()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	t.Log("building test cluster and environment")
+	addons := []clusters.Addon{}
+	addons = append(addons, metallb.New())
+	if b, err := loadimage.NewBuilder().WithImage(imageLoad); err == nil {
+		addons = append(addons, b.Build())
+	}
+	addons = append(addons, kuma.New())
+	builder := environments.NewBuilder().WithAddons(addons...)
+	if clusterVersionStr != "" {
+		clusterVersion, err := semver.ParseTolerant(clusterVersionStr)
+		require.NoError(t, err)
+		builder.WithKubernetesVersion(clusterVersion)
+	}
+	env, err := builder.Build(ctx)
+	require.NoError(t, err)
+	defer func() {
+		assert.NoError(t, env.Cleanup(ctx))
+	}()
+
+	t.Log("deploying kong components")
+	manifest, err := getTestManifest(t, dblessPath)
+	require.NoError(t, err)
+	deployment := deployKong(ctx, t, env, manifest)
+
+	t.Log("adding Kuma mesh")
+	kuma.EnableMeshForNamespace(ctx, env.Cluster(), "kong")
+	kuma.EnableMeshForNamespace(ctx, env.Cluster(), "default")
+
+	// scale to force a restart of pods and trigger mesh injection (we can't annotate the Kong namespace in advance,
+	// it gets clobbered by deployKong()). is there a "rollout restart" in client-go? who knows!
+	scale := &autoscalingv1.Scale{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      deployment.Name,
+			Namespace: deployment.Namespace,
+		},
+		Spec: autoscalingv1.ScaleSpec{
+			Replicas: 0,
+		},
+	}
+	_, err = env.Cluster().Client().AppsV1().Deployments(deployment.Namespace).UpdateScale(ctx,
+		deployment.Name, scale, metav1.UpdateOptions{})
+	require.NoError(t, err)
+	scale.Spec.Replicas = 2
+	_, err = env.Cluster().Client().AppsV1().Deployments(deployment.Namespace).UpdateScale(ctx,
+		deployment.Name, scale, metav1.UpdateOptions{})
+	require.NoError(t, err)
+
+	t.Log("running ingress tests to verify all-in-one deployed ingress controller and proxy are functional")
+	deployIngress(ctx, t, env)
 	verifyIngress(ctx, t, env)
 }
 
